45a970077f5e2ce1d7d66c002118be66cd47e3db
Sungho <hurgj123kr@gmail.com>
2022-06-25 20:58:22 +0900

    report.html 작성

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, redirect, requests
 
 app = Flask("Flask_Web_Scrapper")
 
@@ -6,5 +6,13 @@ app = Flask("Flask_Web_Scrapper")
 def home():
     return render_template("home.html")
 
+@app.route("/report", methods=["GET"])
+def results():
+    word = requests.args.get('word')
+    if word:
+        word = word.lower()
+    else:
+        return redirect("home.html")
+    return render_template("report.html",job=word)
 
 app.run(host="127.0.0.1")

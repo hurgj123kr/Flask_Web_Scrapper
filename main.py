f2f34a5582b5a9847abd906d3a60c899a1ac9ab9
Sungho <hurgj123kr@gmail.com>
2022-06-25 15:25:30 +0900

    home.html 간략히 구현

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template
 
 app = Flask("Flask_Web_Scrapper")
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-app.run(host='localhost')
+app.run(host="127.0.0.1")

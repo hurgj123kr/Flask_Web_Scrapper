c8ec8f656b47bb79dac2ccd014c97175474b6063
Sungho <hurgj123kr@gmail.com>
2022-06-27 11:24:06 +0900

    exporter 구현 완료

---
+++ exporter.py
@@ -0,0 +1,9 @@
+import csv
+
+def save_to_file(jobs,word):
+    file = open(f"{word}.csv",mode="w") #파일 저장 및 이 파일은 쓰기만 가능
+    writer = csv.writer(file) # csv파일 읽기
+    writer.writerlow(['Title','Company','Location','Link']) # writer 행에 들어갈 내용
+    for job in jobs:
+        writer.writerlow(list(job.values())) #  첫 행 밑에 들어갈 내용.
+    return 

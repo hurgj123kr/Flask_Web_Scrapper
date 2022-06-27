d376b3c09ccf5a618140218d77b891dff71b7f17
Sungho <hurgj123kr@gmail.com>
2022-06-27 12:21:40 +0900

    exporter 오타 수정

--- exporter.py
+++ exporter.py
@@ -3,7 +3,8 @@ import csv
 def save_to_file(jobs,word):
     file = open(f"{word}.csv",mode="w") #파일 저장 및 이 파일은 쓰기만 가능
     writer = csv.writer(file) # csv파일 읽기
-    writer.writerlow(['Title','Company','Location','Link']) # writer 행에 들어갈 내용
+    writer.writerrow(['Title','Company','Location','Link']) # writer 행에 들어갈 내용
     for job in jobs:
-        writer.writerlow(list(job.values())) #  첫 행 밑에 들어갈 내용.
+        writer.writerrow(list(job.values())) #  첫 행 밑에 들어갈 내용.
     return 
+

43ee71903d86e6f77408242b68799743252210d7
Sungho <hurgj123kr@gmail.com>
2023-03-09 21:30:37 +0900

    db pymysql connect 1

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import os
+import scipy.io
+import csv
+import pymysql
+from dotenv import load_dotenv,find_dotenv
 
-print(os.getcwd())
-os.chdir("/Flask_Web_Scrapper")
-print(os.getcwd())
+#env file find
+load_dotenv(find_dotenv())
 def search(dirname):
     try:
         filenames = os.listdir(dirname)
@@ -14,36 +17,41 @@ def search(dirname):
                 out = os.path.splitext(full_filename)[-1]
                 if out == '.csv':
                     print(full_filename)
+                    return full_filename
     except PermissionError:
         pass
 
-# search("Flask_Web_Scrapper")
-#import os
-#import scipy.io
-#import csv
-#import pymysql
-#
-## 로컬 mysql과 커넥션 수행
-#conn = pymysql.connect(host='127.0.0.1', user='root', password='153123gj@#', db='FLASK_test', charset='utf8')
+# 로컬 mysql과 커넥션 수행
+conn = pymysql.connect(host='127.0.0.1', user='root', password=os.getenv('DB_PASSWORD'), db='FLASK_test', charset='utf8')
+cursor = conn.cursor()
+query = "SELECT Title,Company,Location,Link FROM jobs"
+cursor.execute(query)
+
+result = cursor.fetchmany()
+print(result)
+data = {}
+#for i in result:
+#    print(i)
+conn.close()
 #curs = conn.cursor()
 #conn.commit()
 #
 ## CSV 파일 경로...
-#f = open(r'/Users/sunghoheo/Flask_Web_Scrapper/static/csv/java.csv')
+#f = open(search("/Users/sunghoheo/Flask_Web_Scrapper"))
 #csvReader = csv.reader(f)
 #
 ## 컬럼 매핑
 #for row in csvReader:
-#    Title = (row[0])
-#    Company = (row[1])
-#    Location = (row[2])
-#    Link = (row[3])
-#    print(Title)
-#    print(Company)
-#    print(Location)
-#    print(Link)
-#    sql = """insert into jobs (Title, Company, Location, Link) values (%s, %s, %s, %s) """
-#    curs.execute(sql, (Title, Company, Location, Link ))
+#   Title = (row[0])
+#   Company = (row[1])
+#   Location = (row[2])
+#   Link = (row[3])
+#   print(Title)
+#   print(Company)
+#   print(Location)
+#   print(Link)
+#   sql = """insert into jobs (Title, Company, Location, Link) values (%s, %s, %s, %s) """
+#   curs.execute(sql, (Title, Company, Location, Link ))
 #
 ## DB의 변화 저장
 #conn.commit()
